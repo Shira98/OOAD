9b6bff173489c50a64578adf041b5d879c7750f9
anmoljindal13 <anmol.jindal13@gmail.com>
2018-03-30 21:31:40 +0530

    Delete Regualr_Employee.vb

--- Regualr_Employee.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Regualr_Employee
-    Private Sub Regualr_Employee_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click, Button2.Click
-
-    End Sub
-End Class
